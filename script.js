19bae1b899d274b8f30415571468266e23950608
BenRi18 <122071689+BenRi18@users.noreply.github.com>
2024-07-01 14:48:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    const totalSlides = Math.ceil(slides.length - 2);
+
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    
+    const offset = -currentSlide * 100 / 3;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Automatically switch slides every 5 seconds
+setInterval(() => {
+    nextSlide();
+},10000);
